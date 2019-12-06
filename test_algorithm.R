ea34c36c2ce8ed70255207677fdfa2e9d19ff04a
Marianna Plesiak <plesiakmarianna207@ukr.net>
2019-12-06 12:33:26 +0100

    add tukey depth

--- test_algorithm.R
+++ test_algorithm.R
@@ -1,32 +1,66 @@
 #### Algorithm 2
-# Test algorithm for any query point x
+# Test algorithm for query points x
 
 evaluate_depth <- function(data, 
                            halfspaces, 
                            metric = c("mass", "depth")) {
   
   metric <- match.arg(metric, c("mass", "depth"))
-  hm_new <- rep(0, dim(data)[1])
-  n_halfspaces <- dim(halfspaces$directions)[1]
+  checkmate::assert_character(metric, len = 1)
   
-  # extract necessary information from <halfspaces> object
+  # extract necessary information from the <halfspaces> object
+  n_halfspaces <- dim(halfspaces$directions)[1]
   directions <- halfspaces$directions
+  trained_projections <- halfspaces$projections
   split_points <- halfspaces$split_points
   left_weights <- halfspaces$left_weights
   right_weights <- halfspaces$right_weights
   
+  if (metric == "mass") {
+    hm_new <- get_halfspace_mass(data, n_halfspaces, 
+                                 directions, split_points,
+                                 left_weights, right_weights)
+    return(hm_new)
+  } 
+  
+  if (metric == "depth") {
+    hm_new <- rep(Inf, dim(data)[1])
+    for (i in seq_len(n_halfspaces)) {
+      # project all new points on a random direction
+      data_projected <- project_data(directions[i, ], data)
+      # take one projection which was trained before
+      projection_i <- trained_projections[i, ]
+      # update halfspace mass every time when less points on each side of 
+      # the projection are found
+      hm_new <- get_tukey_depth(hm_new, data_projected, projection_i)
+    }
+    return(hm_new)
+  }
+}
+
+get_halfspace_mass <- function(data, n_halfspaces,
+                               directions, split_points,
+                               left_weights, right_weights) {
+  hm_new <- rep(0, dim(data)[1])
   for (i in seq_len(n_halfspaces)) {
     data_projected <- project_data(directions[i, ], data)
-    
     # for each point in data set
     # define on which side from the current split point it lies
     on_left <- (data_projected < split_points[i])
-    
     # add left weights if data point is on the left from the split point
     hm_new[on_left] <- hm_new[on_left] + left_weights[i]
     # otherwise add right weights
     hm_new[!on_left] <- hm_new[!on_left] + right_weights[i]
   }
-  
   hm_new / n_halfspaces
+}
+
+
+get_tukey_depth <- function(hm_new, data_projected, projection_i) {
+  for (k in seq_len(length(data_projected))) {
+    # find minimum of new data points which are on each side of the projection
+    min_points <- sum(projection_i > data_projected[k])
+    hm_new[k] <- min(hm_new[k], min_points)
+  }
+  hm_new
 }
--- train_algorithm.R
+++ train_algorithm.R
@@ -1,45 +1,44 @@
-#### Algorithm 1 
+#### Algorithm 1
 # Train halfspace mass
 
 train_depth <- function(
-  data,                 # data set to train on
-  n_halfspace = 1L,     # number of simulated halfspaces; t in Chen et al.
-  subsample = 1L,       # subsample fraction of data set; D(i) in Chen et al.
-  scope = 1L,           # size parameter; lambda in Chen et al.
+  data,              # data set to train on
+  n_halfspace = 1L,  # number of simulated halfspaces; t in Chen et al.
+  subsample = 1L,    # subsample fraction of data set; D(i) in Chen et al.
+  scope = 1L,        # size parameter; lambda in Chen et al.
   seed = 1) {
-  
   check_inputs(data, n_halfspace, subsample, scope, seed)
   
   data_dim <- dim(data)[2]
   n_subsample <- ceiling(nrow(data) * subsample)
   
-  # create empty matrix for generated random directions
+  # create empty matrix for generated random directions and projected data
   directions <- matrix(nrow = n_halfspace, ncol = data_dim)
+  projections <- matrix(nrow = n_halfspace, ncol = n_subsample)
   # create empty vectors for critical points on the generated directions
-  split_vector <- 
-          left <-
-         right <- vector(mode = "numeric", length = n_halfspace)
+  split_vector <- left <- right <-
+    vector(mode = "numeric", length = n_halfspace)
   
   set.seed(seed)
   
   for (i in seq_len(n_halfspace)) {
-    
-    direction <- generate_direction(data_dim) 
-    sampled_data <- get_subsample(data, n_subsample) 
-    projected_subsample <- project_data(direction, sampled_data) 
+    direction <- generate_direction(data_dim)
+    sampled_data <- get_subsample(data, n_subsample)
+    projected_subsample <- project_data(direction, sampled_data)
     split <- get_split(projected_subsample, scope)
-    fraction_left <- sum(projected_subsample < split) / n_subsample 
+    fraction_left <- sum(projected_subsample < split) / n_subsample
     fraction_right <- sum(projected_subsample >= split) / n_subsample
     
     directions[i, ] <- direction
+    projections[i, ] <- projected_subsample
     split_vector[i] <- split
     left[i] <- fraction_left
     right[i] <- fraction_right
-    
   }
   
   HM_list <- list(
     "directions" = directions,
+    "projections" = projections,
     "split_points" = split_vector,
     "left_weights" = left,
     "right_weights" = right
@@ -47,25 +46,28 @@ train_depth <- function(
   HM_list
 }
 
-check_inputs <- function(data, 
-                         n_halfspace, subsample, scope, seed) {
+check_inputs <- function(data, n_halfspace, subsample, scope, seed) {
   library(checkmate)
-  assert(check_data_frame(data, types = c("numeric", "integer"), 
+  assert(check_data_frame(data,
+                          types = c("numeric", "integer"),
                           any.missing = FALSE, min.rows = 1, min.cols = 1),
-         check_matrix(data, mode = c("numeric", "integerish"),
-                      any.missing = FALSE, min.rows = 1, min.cols = 1),
+         check_matrix(data,
+                          mode = c("numeric", "integerish"),
+                          any.missing = FALSE, min.rows = 1, min.cols = 1),
          combine = "or")
   assert_count(n_halfspace, positive = TRUE)
-  assert_numeric(subsample, lower = 0, upper = 1, len = 1, any.missing = FALSE)
+  assert_numeric(subsample, lower = .Machine$double.eps, 
+                 upper = 1, len = 1, any.missing = FALSE)
   assert_numeric(scope, lower = 1)
+  assert_count(seed, positive = TRUE)
 }
 
 
-# generate random direction by random sampling from standard normal distribution
-# normalize direction for the next projection on it
+# generate direction by random sampling from standard normal distribution
+# normalize the direction for the next projection on it
 generate_direction <- function(dimension) {
-  direction <- rnorm(dimension) 
-  normalized_direction <- direction / sqrt(sum(direction ^ 2))
+  direction <- rnorm(dimension)
+  normalized_direction <- direction / sqrt(sum(direction^2))
   normalized_direction
 }
 
@@ -75,8 +77,10 @@ get_subsample <- function(data, n_subsample) {
   data[sampled_rows, , drop = FALSE]
 }
 
-# project given subsample on the direction vector
+# project given subsample data on the direction vector
 project_data <- function(direction, data) {
+  #checkmate::assert_true(all.equal(sum(direction^2), 1L, 
+  #                                 tolerance = .Machine$double.eps))
   projected_data <- as.matrix(data) %*% direction
   projected_data
 }
@@ -84,11 +88,11 @@ project_data <- function(direction, data) {
 # calculate critical points of projected subsample
 # and randomly select s(i) based on these critical points and scope
 get_split <- function(projected_subsample, scope) {
-  # critical points
+  # calculate critical points
   max_point <- max(projected_subsample)
   min_point <- min(projected_subsample)
   mid_point <- (max_point + min_point) / 2
-  # random split point
+  # calculate and return a random split point
   span <- max_point - min_point
   deviation <- (scope * span) / 2
   left_bound <- mid_point - deviation
